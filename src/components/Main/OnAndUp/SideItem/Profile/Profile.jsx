18fe76e39ed15e47643c05d3beeb46a36830471f
ChaeaOH <104357856+ChaeaOH@users.noreply.github.com>
2022-07-31 18:10:07 +0900

    혹시올라가나요...?

---
+++ src/components/Main/OnAndUp/SideItem/Profile/Profile.jsx
@@ -0,0 +1,44 @@
+import React, { useId } from "react";
+import { useSelector } from 'react-redux'
+import './css/Profile.css'
+import userData from '../../../../../userData.json'
+
+const Profile = () => {
+  const userId = useSelector((state) => state.user.id.id)
+  console.log(userId)
+  const user = userData.find((user) => {
+    return user.id == userId;
+  });
+  console.log(user);
+
+  if (!user) {
+    return <div>No user data</div>
+  }
+
+  return (
+    <div className='profile-inner'>
+      <h1 className='profile-title'>Profile</h1>
+      {/*       <div className='profile-title'>Profile</div> */}
+      <div className='profile-container'>
+        <div className='profile-img'>
+        </div>
+        <div className='profile-content'>
+          <div className='profile-name'>
+            <span>Name</span>: {user.name}
+          </div>
+          <div className='profile-department'>
+            <span>Department </span>: {user.department}
+          </div>
+          <div className='profile-number'>
+            <span>Number </span>: {user.number}
+          </div>
+          <div className='profile-email'>
+            <span>Email</span> : {user.email}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
